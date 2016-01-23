90f4892839ea07e01b6d5d0d198034bc2f8c8b70
scaledesk_tushar <tushar@scaledesk.com>
2016-01-23 19:35:05 +0530

    change packages list layout

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,13 @@
 var appControllers = angular.module('starter.controllers', []).controller('packageController',function($scope,$http,serverConfig,$state) {
     $http.get(serverConfig.address + 'api/packages').success(function (response) {
         $scope.packages = response.data;
-        console.log(response.data);
+        packages_length=$scope.packages.length;
+         break_length=packages_length/2;
+        delete packages_length;
+        $scope.first_packages_row = $scope.packages.slice(0, break_length);
+        $scope.second_packages_row = $scope.packages.slice(break_length + 1);
+        delete $scope.packages;
+        delete break_length;
     }).error(function (data) {
         console.log("Some error occurred");
         console.log(data);
